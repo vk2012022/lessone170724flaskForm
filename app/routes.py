5ab5ce8f1ad8c4bcd3a55bca2c8f61c4878d7f64
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-07-18 14:51:50 +0300

    add new code 180724

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,16 @@ main = Blueprint('main', __name__)
 
 users = []
 
+def get_age_suffix(age):
+    if 11 <= age % 100 <= 14:
+        return 'лет'
+    elif age % 10 == 1:
+        return 'год'
+    elif 2 <= age % 10 <= 4:
+        return 'года'
+    else:
+        return 'лет'
+
 @main.route('/')
 def index():
     return render_template('index.html', users=users)
@@ -11,8 +21,9 @@ def index():
 @main.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
-    age = request.form.get('age')
+    age = int(request.form.get('age'))
     city = request.form.get('city')
     hobby = request.form.get('hobby')
-    users.append({'name': name, 'age': age, 'city': city, 'hobby': hobby})
+    age_suffix = get_age_suffix(age)
+    users.append({'name': name, 'age': age, 'age_suffix': age_suffix, 'city': city, 'hobby': hobby})
     return redirect(url_for('main.index'))
